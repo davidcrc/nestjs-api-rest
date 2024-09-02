0b5657af8008b993ff8ca98a242b9858d865ba04
davidcrc <david_drcr@hotmail.com>
2024-09-02 09:37:53 -0500

    feat: add create task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -13,5 +21,10 @@ export class TasksController {
   @Get(':id')
   async getTask(@Param('id', ParseIntPipe) id: number) {
     return await this.tasksService.getTask(id);
+  }
+
+  @Post()
+  async createTask(@Body() task: CreateTaskDto) {
+    return await this.tasksService.createTask(task);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -43,6 +44,28 @@ export class TasksService {
         throw new NotFoundException(error.message);
       }
 
+      if (error instanceof Error) {
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  async createTask(task: CreateTaskDto) {
+    const { title, description } = task;
+
+    try {
+      const newTask = await this.prismaService.task.create({
+        data: {
+          title,
+          description,
+        },
+      });
+
+      return newTask;
+    } catch (error) {
       if (error instanceof Error) {
         throw new HttpException(
           error.message,
